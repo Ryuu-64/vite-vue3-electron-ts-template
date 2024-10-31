fc06a86628a49a1fd9a6103a910b09b7a5e0803d
Ryuu <2357622935@qq.com>
2024-10-31 19:13:28 +0800

    feat: about and dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,16 @@
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
-import {isDarkMode, toggleDarkMode} from "./composables";
+import DarkMode from "./components/DarkMode.vue";
 </script>
 
 <template>
-  <el-divider>icon</el-divider>
-  <img alt="vite-logo" width="120" src="./assets/images/vite.svg"/>
-  <img alt="vue-logo" width="120" src="./assets/images/logo.png"/>
-  <img alt="electron-logo" width="120" src="./assets/images/electron.png"/>
-  <img alt="ts-logo" width="120" src="./assets/images/ts.png"/>
-  <el-divider>component</el-divider>
-  <HelloWorld msg="Vite + Vue 3 + Electron + TypeScript"/>
-  <el-divider>dark mode</el-divider>
-  <p>
-    <el-button @click="toggleDarkMode()">Toggle Dark Mode</el-button>
-  </p>
-  <p>
-    <el-text size="large">is dark mode: {{ isDarkMode }}</el-text>
-  </p>
-  <el-divider>Plugins</el-divider>
-  <img alt="pinia logo" width="120" src="./assets/images/pinia.svg"/>
-  <img alt="element-plus-logo" width="240" src="./assets/images/element-plus-logo.svg"/>
+  <hello-world msg="Vite + Vue 3 + Electron + TypeScript"/>
+  <dark-mode/>
 </template>
 
 <style>
 #app {
   text-align: center;
-}
-
-img {
-  margin: 5px;
 }
 
 </style>

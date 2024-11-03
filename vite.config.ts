9c13c7b0f0e4158310ac5a2afe4bb8ad76185f2d
Ryuu-64 <64ryuu@gmail.com>
2024-11-04 01:08:10 +0800

    fix: project file path error in vite.config.ts, src/vue/index.html, tsconfig.json chore: change database url chore: .gitignore add sqlite ignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,26 @@
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
+import * as path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    root: "./src/vue",
     plugins: [vue()],
-    base: './',
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src/vue'),
+        },
+    },
+    build: {
+        rollupOptions: {
+            input: './src/vue/index.html',
+        },
+        outDir: path.resolve(__dirname, 'dist'),
+    },
     server: {
         hmr: {
             overlay: false
         }
-    }
+    },
+    base: './'
 });
